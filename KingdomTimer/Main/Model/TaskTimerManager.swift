6340b2a46e02565c5f06d900e9a90629119007c2
jsjphone8 <jsjphone8@gmail.com>
2021-05-09 23:55:52 +0900

    feat: 타이머 관리 클래스 인터페이스 구현
    TaskTimer를 관리할 클래스의 인터페이스를 구현함.
    필요한 인터페이스를 다 담았는지는 확인이 더 필요함.

---
+++ KingdomTimer/Main/Model/TaskTimerManager.swift
@@ -0,0 +1,53 @@
+//
+//  TaskTimerManager.swift
+//  KingdomTimer
+//
+//  Created by 정성훈 on 2021/05/09.
+//
+
+import Foundation
+
+struct TaskTimerManager {
+    static private var taskTimers = [TaskTimer]()
+    
+    static func startAll() {
+        
+    }
+    
+    static func startAllWithOptimization() {
+        
+    }
+    
+    static func pauseWithOptimization() {
+        
+    }
+    
+    static func start(at index: Int) {
+        
+    }
+    
+    static func pause(at index: Int) {
+        
+    }
+    
+    static func reset(at index: Int) {
+        
+    }
+ 
+    // MARK:- CRUD
+    static func create(timerData: TimerData) {
+        
+    }
+    
+    static func remove(at index: Int) {
+        
+    }
+    
+    static func update(at index: Int, timerData: TimerData) {
+        
+    }
+    
+    static func taskTimer(at index: Int) -> TaskTimer? {
+        return nil;
+    }
+}
