a32a92be07239d846787d33da16a4b72dc90c4e4
jsjphone8 <jsjphone8@gmail.com>
2021-05-18 23:44:03 +0900

    refactor: 타이머 관리 클래스 싱글톤으로 변경

--- KingdomTimer/Main/Model/TaskTimerManager.swift
+++ KingdomTimer/Main/Model/TaskTimerManager.swift
@@ -7,63 +7,64 @@
 
 import Foundation
 
-struct TaskTimerManager {
-    static private var taskTimers = [TaskTimer]()
-    static private let taskTimerDao = TaskTimerDAO()
+class TaskTimerManager {
+    static var shared = TaskTimerManager()
+    private var taskTimers = [TaskTimer]()
+    private let taskTimerDao = TaskTimerDAO()
     
-    static func startAll() {
-        for timer in taskTimers {
+    func startAll() {
+        for timer in self.taskTimers {
             timer.start()
         }
     }
     
-    static func startAllWithOptimization() {
-        for timer in taskTimers {
+    func startAllWithOptimization() {
+        for timer in self.taskTimers {
             timer.startWithOptimization()
         }
     }
     
-    static func pauseWithOptimization() {
-        for timer in taskTimers {
+    func pauseWithOptimization() {
+        for timer in self.taskTimers {
             timer.startWithOptimization()
         }
     }
     
-    static func start(at index: Int) {
+    func start(at index: Int) {
         guard !self.taskTimers.isEmpty && index < self.taskTimers.count else {
             return
         }
-        taskTimers[index].start()
+        self.taskTimers[index].start()
     }
     
-    static func pause(at index: Int) {
+    func pause(at index: Int) {
         guard !self.taskTimers.isEmpty && index < self.taskTimers.count else {
             return
         }
-        taskTimers[index].pause()
+        self.taskTimers[index].pause()
     }
     
-    static func reset(at index: Int) {
+    func reset(at index: Int) {
         guard !self.taskTimers.isEmpty && index < self.taskTimers.count else {
             return
         }
-        taskTimers[index].reset()
+        self.taskTimers[index].reset()
     }
  
     // MARK:- CRUD
-    static func create(data: TimerDataInputContainer) {
+    func create(data: TimerDataInputContainer) {
         guard let timer = taskTimerDao.create(data: data) else { return }
         self.taskTimers.append(timer)
     }
     
-    static func taskTimer(at index: Int) -> TaskTimer? {
+    func taskTimer(at index: Int) -> TaskTimer? {
         guard !self.taskTimers.isEmpty && index < self.taskTimers.count else {
             return nil
         }
         return self.taskTimers[index]
     }
     
-    static func update(at index: Int, newTimerData timerData: TimerDataInputContainer) -> Bool {
+    func update(at index: Int, newTimerData timerData: TimerDataInputContainer) -> Bool {
         guard !self.taskTimers.isEmpty && index < self.taskTimers.count else {
             return false
         }
@@ -78,7 +79,7 @@ struct TaskTimerManager {
         return true
     }
     
-    static func remove(at index: Int) -> Bool {
+    func remove(at index: Int) -> Bool {
         guard !self.taskTimers.isEmpty && index < self.taskTimers.count else {
             return false
         }
